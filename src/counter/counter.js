5f0f3a06169635a5b73be833b46097ff3437ed6e
TechNguyen <ndt13102003@gmail.com>
2023-01-29 21:59:22 +0700

    first-time

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -1,33 +1,55 @@
 import classNames from 'classnames/bind'
 import Style from './counter.module.scss'
 import { useState } from 'react'
-import JSONDATA from './MOCK_DATA.json'
 const cx = classNames.bind(Style)
+const sexs = [
+    { id: 1, value: 'Nam' },
+
+    { id: 2, value: 'Nu' },
+
+    { id: 3, value: 'Khac' },
+]
 function Counter() {
-    const [input, setInput] = useState('')
+    const [checked, setCheked] = useState(1)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const handleRegister = () => {
+        console.log({ name, email, id: checked })
+    }
     return (
         <div className={cx('counter-wrapper')}>
-            <div className={cx('counter-content')}>
-                <input
-                    placeholder="...Search"
-                    onChange={(e) => {
-                        setInput(e.target.value)
-                    }}
-                />
-                <div className={cx('data-list')}>
-                    {JSONDATA.filter((data) => {
-                        if (input == '') {
-                            return
-                        } else if (
-                            data.first_name.toUpperCase().includes(input.toUpperCase())
-                        ) {
-                            return data
-                        }
-                    }).map((data) => (
-                        <p key={data.id}>{data.first_name}</p>
-                    ))}
-                </div>
+            <label for="name">Full name</label>
+            <input
+                onChange={(e) => {
+                    setName(e.target.value)
+                }}
+                type="text"
+                className={cx('name')}
+            />
+            <label for="email">Email</label>
+            <input
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                }}
+                type="email"
+                className={cx('email')}
+            />
+            <div className={cx('sex')}>
+                {sexs.map((sex) => (
+                    <span key={sex.id}>
+                        <input
+                            type="radio"
+                            checked={checked === sex.id}
+                            onChange={() => setCheked(sex.id)}
+                            className={cx('input-sex')}
+                        />
+                        {sex.value}
+                    </span>
+                ))}
             </div>
+            <button onClick={handleRegister} className={cx('register')}>
+                Register
+            </button>
         </div>
     )
 }
